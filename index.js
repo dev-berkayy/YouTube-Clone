324d2020e0986e8a026148644eda596945d9deda
juniorberkay <7ubisoft@gmail.com>
2023-12-18 18:59:13 +0300

    YOU TUBE clon

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+function openside() {
+    let side = document.querySelector(".left-btns")
+
+    side.classList.toggle("active")
+
+}
+
+
+function input() {
+    var input, filter, videos;
+
+    filter.
+}
+
+
