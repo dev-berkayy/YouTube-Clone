9f990386c860d8bc4d8275c65a5b9a0bfc39df05
juniorberkay <7ubisoft@gmail.com>
2023-12-21 18:38:53 +0300

    Upgrade My you tube web site

--- index.js
+++ index.js
@@ -2,18 +2,41 @@
 
 
 
+
+
+
+
+const filterButtons = document.querySelectorAll(".filter_buttons button")
+const filterableCards = document.querySelectorAll(".filterable_cards a")
+
+const filterCards = (e) => {
+    document.querySelector(".active").classList.remove("active")
+    e.target.classList.add("active")
+
+    filterableCards.forEach(a => {
+        a.classList.add("hide")
+
+        if (a.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+            a.classList.remove("hide")
+        }
+    })
+}
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
+
+
 function openside() {
-    let side = document.querySelector(".left-btns")
-
-    side.classList.toggle("active")
-
+    let element = document.querySelector(".left-btns");
+    let btn = document.querySelector(".left-btns2");
+    btn.classList.toggle("activex3")
+    element.classList.toggle("activex")
 }
 
 
-function input() {
-    var input, filter, videos;
 
-    filter.
-}
+
+
+
+
 
 
