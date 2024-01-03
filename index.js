bf0b90cf697b9997fd88045c5d64e3b1da556b63
juniorberkay <7ubisoft@gmail.com>
2024-01-03 12:46:36 +0300

    update   json

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 
-let data;
 
+
+
+let data;
 
 (async () => {
     try {
         const response = await fetch("./data.json")
         data = await response.json();
     } catch (error) {
-        console.log(error)
         data = []
-
     } finally {
         main()
     }
 })()
 
 
-
-
-
 function main() {
-    let row = document.querySelector("row")
-    row.appendChild(data)
-    console.log(data)
+    let row = document.querySelector(".row")
+    data.forEach(item => {
+        let card = document.createElement("a")
+        card.className = "cards-content";
+
+        card.innerHTML = `
+<video id="videos" src = "${item.video}"></video>
+<div class="card_body">
+    <img id="card-img" src="${item.channel.avatar}" alt="">
+    <div class="content-card">
+        <p class="card_text">
+        ${item.title}
+        </p>
+
+        <p class="card-text2">${item.channel.name} <br>  <span style="margin-top: 6px;">
+        ${item.views} watch ' ${item.date}
+            </span> </p>
+
+    </div>
+
+</div>
+`
+        row.appendChild(card)
+
+    })
 }
-
-
-
 
 function openside() {
     let element = document.querySelector(".left-btns");
@@ -42,5 +58,34 @@ function openside() {
 }
 
 
-// let params = new URLSearchParams(document.location.search);
-// params.get("videoId");
+function searchFunction() {
+    const input = document.querySelector("input").value.toUpperCase();
+    const cardcontainer = document.querySelector(".row")
+    const cards = cardcontainer.getElementsByClassName("cards-content")
+
+
+    for (i = 0; i < cards.length; i++) {
+        let title = cards[i].querySelector(".card_text")
+
+
+        if (title.innerText.toUpperCase().indexOf(input) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
+
+let videos = document.querySelectorAll("#videos");
+
+videos.forEach((video) => {
+    video.addEventListener("mouseover", function () {
+        video.play();
+    });
+
+    video.addEventListener("mouseleave", function () {
+        video.pause();
+        video.currentTime = 0; // Reset the video to the beginning on mouse leave
+    });
+});
